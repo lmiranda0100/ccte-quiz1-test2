f885a83248b18614c9e0069af6eff908630a859a
LMiranda <skills21social@gmail.com>
2024-12-18 18:06:16 +0000

    1st commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import "./styles.css";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+// Use non-null assertion operator to ensure the root element exists
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
     <App />
